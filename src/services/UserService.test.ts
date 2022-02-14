8d991c56bf5b67c3629f54bc89c9c7d92b917b1c
Peterson Adami Candido <contact.petersonac@protonmail.com>
2022-02-14 18:46:48 -0300

    new user tests

--- src/services/UserService.test.ts
+++ src/services/UserService.test.ts
@@ -1,3 +1,4 @@
+import CreateUserArguments from '@interfaces/User/CreateUserArguments';
 import { User } from '@prisma/client';
 import prismaMock from '@utils/prisma.test';
 import UserService from './UserService';
@@ -12,13 +13,31 @@ describe('User Service', () => {
     username: 'username',
   };
 
-  it('Should be able to create a user', async () => {
+  const createUserArguments: CreateUserArguments = {
+    email: createdUser.email,
+    password: createdUser.password,
+    username: createdUser.username,
+  };
+
+  it('should be able to create a user', async () => {
     prismaMock.user.create.mockResolvedValue(createdUser);
 
-    await expect(UserService.create({
-      email: createdUser.email,
-      password: createdUser.password,
-      username: createdUser.username,
-    })).resolves.toEqual(createdUser);
+    await expect(UserService.create(createUserArguments)).resolves.toEqual(createdUser);
+  });
+
+  it('should not be able to create a user with same username', async () => {
+    prismaMock.user.findFirst.mockResolvedValue({
+      ...createdUser,
+    });
+
+    await expect(UserService.create(createUserArguments)).rejects.toEqual(new Error('username already taken'));
+  });
+
+  it('should not be able to create a user with same email', async () => {
+    prismaMock.user.findFirst
+      .mockResolvedValueOnce(null)
+      .mockResolvedValueOnce({ ...createdUser });
+
+    await expect(UserService.create(createUserArguments)).rejects.toEqual(new Error('email already in use'));
   });
 });
